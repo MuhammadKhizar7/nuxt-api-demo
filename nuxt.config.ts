b4bab4631fb5f629cd4227f9154323ca24f7d4c1
MuhammadKhizar7 <muhammadkhizar7@gmail.com>
2024-12-27 23:20:03 +0500

    fix: open api meta placement issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,14 @@ export default defineNuxtConfig({
     experimental: {
       openAPI: true,
       database: true
+    },
+    openAPI: {
+    meta: {
+      title: 'My Awesome Project',
+      description: 'This might become the next big thing.',
+      version: '1.0'
     }
   },
-  openAPI: {
-  meta: {
-    title: 'My Awesome Project',
-    description: 'This might become the next big thing.',
-    version: '1.0'
-  }
 }
 
 })
